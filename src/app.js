938474de1b8d229fe2dd0ed05789d9acbcec2a3e
Robert Novotny <novotnr0@gmail.com>
2019-10-29 22:22:58 +0100

    Pridane asynchronne dotahovanie majitelov albumov
    Pre kazdy album spustime samostatny request, promisy zozbierame
    a pockame na ne pomocou Promise.all.

--- src/app.js
+++ src/app.js
@@ -2,8 +2,25 @@ function showResult(response) {
     document.getElementById('result').textContent = response
 }
 
+function associate(albums, users) {
+    albums.forEach((album, i) => album.user = users[i]);
+    return albums
+}
+
+function findUserByAlbumAsync(album) {
+    return fetch(`https://jsonplaceholder.typicode.com/users/${album.userId}`)
+        .then(response => response.json())
+}
+
+function fetchUsersAsync(albums) {
+    let userPromises = albums.map(findUserByAlbumAsync);
+    return Promise.all(userPromises)
+        .then(users => associate(albums, users))
+}
+
 fetch('http://jsonplaceholder.typicode.com/albums')
     .then(response => response.json())
+    .then(fetchUsersAsync)
     .then(JSON.stringify)
     .then(showResult)
     .catch(showResult);
